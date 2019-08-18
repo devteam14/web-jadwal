56cc32c80a4848edb948df2fda0709b25230ee90
Ilyas DEMIRTAS <ilyasdemirtas@hotmail.com.tr>
2019-08-18 23:41:24 +0300

    Generated calendar page for user.

--- src/components/Students/Show.vue
+++ src/components/Students/Show.vue
@@ -3,57 +3,37 @@
     <v-layout>
         <v-flex>
             <v-card :raised="raised" :elevation="elevation">
+                <v-card-text>
+                    <b>Student:</b> {{ this.student.name }}<br />
+                    <b>Class:</b> {{ this.student.class }}<br />
+                    <b>Exam Count:</b> {{ this.student.exam_count }}<br />
+                    </v-card-text>
+            </v-card>
+            <v-card :raised="raised" :elevation="elevation">
                 <v-card-title class="py-4">
                     <v-flex xs12 sm6 md3>
-                        <v-text-field v-model="search" append-icon="search" label="Search" single-line outlined hide-details></v-text-field>
+                        <v-text-field
+                            v-model="search"
+                            append-icon="search"
+                            label="Search"
+                            single-line
+                            outlined
+                            :v-model="search"
+                            @input="filterData()"
+                        ></v-text-field>
                     </v-flex>
-                    <v-spacer></v-spacer>
-                    <v-btn outlined color="primary" dark v-on="on">
-                        <v-icon>save_alt</v-icon>Export
-                    </v-btn>
-
                 </v-card-title>
-                <v-data-table multi-sort :search="search" :headers="headers" :items="data" :page.sync="pagination.page" :items-per-page="pagination.itemsPerPage" @page-count="pageCount = $event">
-
-                    <template v-slot:item.student="props">
-                        <v-edit-dialog :return-value.sync="props.item.student" large persistent @save="save" @cancel="cancel" @open="open" @close="close">
-                            <div>{{ props.item.student }}</div>
-                            <template v-slot:input>
-                                <div class="mt-4 title">Update Student</div>
-                            </template>
-                            <template v-slot:input>
-                                <v-text-field v-model="props.item.student" :rules="[max25chars]" label="Edit" single-line counter autofocus></v-text-field>
-                            </template>
-                        </v-edit-dialog>
-                    </template>
-                    <template v-slot:item.stuClass="props">
-                        <v-edit-dialog :return-value.sync="props.item.stuClass" large persistent @save="save" @cancel="cancel" @open="open" @close="close">
-                            <div>{{ props.item.stuClass }}</div>
-                            <template v-slot:input>
-                                <div class="mt-4 title">Update Studen Class</div>
-                            </template>
-                            <template v-slot:input>
-                                <v-text-field v-model="props.item.stuClass" :rules="[max25chars]" label="Edit" single-line counter autofocus></v-text-field>
-                            </template>
-                        </v-edit-dialog>
-                    </template>
-                    <template v-slot:item.girecegi_sinav_sayisi="props">
-                        <v-edit-dialog :return-value.sync="props.item.girecegi_sinav_sayisi" large persistent @save="save" @cancel="cancel" @open="open" @close="close">
-                            <div>{{ props.item.girecegi_sinav_sayisi }}</div>
-                            <template v-slot:input>
-                                <div class="mt-4 title">Update Exam Count</div>
-                            </template>
-                            <template v-slot:input>
-                                <v-text-field v-model="props.item.girecegi_sinav_sayisi" :rules="[max25chars]" label="Edit" single-line counter autofocus></v-text-field>
-                            </template>
-                        </v-edit-dialog>
+                <v-data-table disable-sort hide-default-footer :headers="headers" :items="data" class="calendar">
+                    <template v-slot:item="{ headers, item }">
+                        <tr>
+                            <td v-for="header in headers" v-html="item[header.value]" align="left" valign="top"></td>
+                        </tr>
                     </template>
                 </v-data-table>
                 <v-snackbar v-model="snack" :timeout="3000" :color="snackColor">
                     {{ snackText }}
                     <v-btn text @click="snack = false">Close</v-btn>
                 </v-snackbar>
-
             </v-card>
         </v-flex>
     </v-layout>
@@ -61,56 +41,91 @@
 </template>
 
 <script>
-import json_data from '../../data/students.json';
+var _ = require('lodash');
 
 export default {
     data: function() {
         return {
+            search: '',
             on: false,
             elevation: 0,
             raised: false,
-            search: '',
             snack: false,
             snackColor: '',
             snackText: '',
-            max25chars: v => v.length <= 25 || 'Input too long!',
-            pagination: {
-                page: 1,
-                itemsPerPage: 10
-            },
-            headers: [{
-                    text: 'Student',
-                    value: 'student',
-                },
-                {
-                    text: 'Student Class',
-                    value: 'stuClass'
-                },
-                {
-                    text: 'Exam Count',
-                    value: 'girecegi_sinav_sayisi'
-                }
-            ],
-            data: json_data
+            headers: [],
+            data: [],
+            tmp_data: [],
+            student: []
         }
     },
     created: function() {
-        this.pagination.totalItems = this.data.length;
-    },
-    computed: {
-        pages: function() {
-            if (
-                this.pagination.itemsPerPage == null ||
-                this.pagination.totalItems == null
-            )
-                return 0;
-
-            return Math.ceil(
-                this.pagination.totalItems / this.pagination.itemsPerPage
-            );
-        }
+        this.item_id = this.$router.currentRoute.params.id;
+        this.getData();
     },
     methods: {
+        getData(){
+            this.$http
+            .get(utils.apiBaseUrl + "students/" + this.item_id)
+            .then(resp => {
+                this.tmp_data = resp.data.data.exams;
+                this.preparingData(this.tmp_data);
+            })
+            .catch(err => {
+                this.snack = true;
+                this.snackColor = "error";
+                this.snackText = "Opps! Something went wrong when getting data.";
+            });
+        },
+        preparingData(exam_data){
+            
+            var new_data = [];
+            var new_headers = [];
+            new_headers.push({text: 'Hours', value: 'hours', align: 'center'});
+            
+            var exams = exam_data;
+            for(var hour in exams) {
+                
+                var obj = {};
+                const days = exams[hour];
+                for(var day in days){
+
+                    if(hour == 9){
+                        new_headers.push({ text: day, value: day });
+                    }
+
+                    obj['hours'] = hour;
+                    var filtered = days[day].filter(function (el) {
+                        return el != null;
+                    });
+                    obj[day] = filtered.join('<br>');
+            
+                }
+                new_data.push(obj);
+            }
+
+            this.headers = new_headers;
+            this.data = new_data;
+        },
+        filterData(){
+            var data = _.cloneDeep(this.tmp_data);
+            if(this.search != ''){
+                
+                for (var hour in data){
+                    const days = data[hour];
+                    for(var day in days){
+                        const exams = days[day];
+                        for(var exam in exams){
+                            if (exams[exam] != this.search){
+                                delete data[hour][day][exam];
+                            }
+                        }
+                    }
+                }
+                
+            }
+            this.preparingData(data);
+        },
         save() {
             this.snack = true
             this.snackColor = 'success'
