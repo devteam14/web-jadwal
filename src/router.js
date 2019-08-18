c7a148fac92b03ad385401e1351d30ae66a73774
Ilyas DEMIRTAS <ilyasdemirtas@hotmail.com.tr>
2019-08-18 23:35:54 +0300

    Added routes for calendar pages.

--- src/router.js
+++ src/router.js
@@ -3,11 +3,13 @@ import VueRouter from 'vue-router';
 import Login from '@/components/Auth/Login';
 import Dashboard from '@/components/Dashboard';
 import AcademicStaffList from '@/components/AcademicStaffList/Index.vue';
+import AcademicStaffListShow from '@/components/AcademicStaffList/Show.vue';
 import FulfillmentList from '@/components/FulfillmentList/Index.vue';
 import Rooms from '@/components/Rooms/Index.vue';
 import Lessons from '@/components/Lessons/Index.vue';
 import Organization from '@/components/Organization/Index.vue';
 import ExamSchedule from '@/components/ExamSchedule/Index.vue';
+import ExamScheduleCalendar from '@/components/ExamSchedule/Calendar.vue';
 import Student from '@/components/Students/Index.vue';
 import StudentShow from '@/components/Students/Show.vue';
 import Exams from '@/components/Exams/Index.vue';
@@ -52,9 +54,26 @@ export default new VueRouter({
             }
         },
         {
-            path: '/academic-staff-list',
+            path: '/academic-staff-lists',
             component: AcademicStaffList,
-            name: 'academic_staff_list',
+            name: 'academic_staff_lists',
+            meta: {
+                breadcrumb: [{
+                        text: 'Dashboard',
+                        disabled: false,
+                        to: '/'
+                    },
+                    {
+                        text: 'Academic Staff Lists',
+                        disabled: true
+                    }
+                ]
+            }
+        },
+        {
+            path: '/academic-staff-lists/:id',
+            component: AcademicStaffListShow,
+            name: 'academic-staff-lists',
             meta: {
                 breadcrumb: [{
                         text: 'Dashboard',
@@ -63,6 +82,11 @@ export default new VueRouter({
                     },
                     {
                         text: 'Academic Staff List',
+                        disabled: false,
+                        to: '/academic-staff-lists'
+                    },
+                    {
+                        text: 'Academic Staff List Show',
                         disabled: true
                     }
                 ]
@@ -154,6 +178,23 @@ export default new VueRouter({
             }
         },
         {
+            path: '/exam-schedule-calendar',
+            component: ExamScheduleCalendar,
+            name: 'exam_schedule_calendar',
+            meta: {
+                breadcrumb: [{
+                        text: 'Dashboard',
+                        disabled: false,
+                        to: '/'
+                    },
+                    {
+                        text: 'Exam Schedule Calendar',
+                        disabled: true
+                    }
+                ]
+            }
+        },
+        {
             path: '/students',
             component: Student,
             name: 'students',
@@ -182,441 +223,16 @@ export default new VueRouter({
                     },
                     {
                         text: 'Students',
-                        disabled: true,
-                        to: '/student'
+                        disabled: false,
+                        to: '/students'
                     },
                     {
-                        text: 'Edit',
+                        text: 'Student Show',
                         disabled: true
                     }
                 ]
             }
         },
-        // {
-        //   path: '/users',
-        //   component: User,
-        //   name: 'users',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Users',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/users/:id/edit',
-        //   component: EditUser,
-        //   name: 'user_edit',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'User',
-        //         disabled: false,
-        //         to: '/users'
-        //       },
-        //       {
-        //         text: 'Edit',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/admin-users',
-        //   component: AdminUser,
-        //   name: 'admin-users',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Admin Users',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/admin-users/create',
-        //   component: CreateAdminUser,
-        //   name: 'admin_user_create',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Admin Users',
-        //         disabled: false,
-        //         to: '/admin-users'
-        //       },
-        //       {
-        //         text: 'Create',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/admin-users/:id/edit',
-        //   component: EditAdminUser,
-        //   name: 'admin_user_edit',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Admin User',
-        //         disabled: false,
-        //         to: '/admin-users'
-        //       },
-        //       {
-        //         text: 'Edit',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/campaigns',
-        //   component: Campaigns,
-        //   name: 'campaigns',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Campaigns',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/campaigns/create',
-        //   component: CreateCampaign,
-        //   name: 'campaign_create',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Campaigns',
-        //         disabled: false,
-        //         to: '/campaigns'
-        //       },
-        //       {
-        //         text: 'Create',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/campaigns/:id/edit',
-        //   component: EditCampaign,
-        //   name: 'campaign_edit',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Campaigns',
-        //         disabled: false,
-        //         to: '/campaigns'
-        //       },
-        //       {
-        //         text: 'Edit',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/notifications',
-        //   component: Notifications,
-        //   name: 'notifications',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Notifications',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/notifications/create',
-        //   component: CreateNotification,
-        //   name: 'notification_create',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Notifications',
-        //         disabled: false,
-        //         to: '/notifications'
-        //       },
-        //       {
-        //         text: 'Create',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/notifications/:id/edit',
-        //   component: EditNotification,
-        //   name: 'notification_edit',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Notifications',
-        //         disabled: false,
-        //         to: '/notifications'
-        //       },
-        //       {
-        //         text: 'Edit',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/timelines',
-        //   component: Timelines,
-        //   name: 'timelines',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Timelines',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/timelines/create',
-        //   component: CreateTimeline,
-        //   name: 'timeline_create',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Timelines',
-        //         disabled: false,
-        //         to: '/timelines'
-        //       },
-        //       {
-        //         text: 'Create',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/timelines/:id/edit',
-        //   component: EditTimeline,
-        //   name: 'timeline_edit',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Timelines',
-        //         disabled: false,
-        //         to: '/timelines'
-        //       },
-        //       {
-        //         text: 'Edit',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/surveys',
-        //   component: Surveys,
-        //   name: 'surveys',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Surveys',
-        //         disabled: false
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/surveys/create',
-        //   component: CreateSurvey,
-        //   name: 'survey_create',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Surveys',
-        //         disabled: false,
-        //         to: '/surveys'
-        //       },
-        //       {
-        //         text: 'Create',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/surveys/:id/edit',
-        //   component: EditSurvey,
-        //   name: 'survey_edit',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Surveys',
-        //         disabled: false,
-        //         to: '/surveys'
-        //       },
-        //       {
-        //         text: 'Edit',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/content-categories',
-        //   component: ContentCategories,
-        //   name: 'content_categories',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Content Categories',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/content-categories/create',
-        //   component: CreateContentCategory,
-        //   name: 'content_category_create',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Content Categories',
-        //         disabled: false,
-        //         to: '/content-categories'
-        //       },
-        //       {
-        //         text: 'Create',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/content-categories/:id/edit',
-        //   component: EditContentCategory,
-        //   name: 'content_category_edit',
-        //   meta: {
-        //     breadcrumb: [
-        //       {
-        //         text: 'Dashboard',
-        //         disabled: false,
-        //         to: '/'
-        //       },
-        //       {
-        //         text: 'Contents Categories',
-        //         disabled: false,
-        //         to: '/content-categories'
-        //       },
-        //       {
-        //         text: 'Edit',
-        //         disabled: true
-        //       }
-        //     ]
-        //   }
-        // },
         {
             path: '*',
             redirect: '/'
