62c1e03dc7803eedbb0fb1542156d2f76b424e32
Ilyas DEMIRTAS <ilyasdemirtas@hotmail.com.tr>
2019-08-19 21:24:48 +0300

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:devteam14/web-jadwal.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:devteam14/web-jadwal.git master:gh-pages
+git push -f git@github.com:devteam14/web-jadwal.git master:build
 
 cd -
