3081ec641b341bb935fb4f326eb0b406a8b63fc9
Ilyas DEMIRTAS <ilyasdemirtas@hotmail.com.tr>
2019-08-24 01:47:40 +0300

    Added env. for production

--- src/components/AcademicStaffList/Show.vue
+++ src/components/AcademicStaffList/Show.vue
@@ -1,148 +1,216 @@
 <template>
-<v-container fluid fill-height>
+  <v-container fluid fill-height>
     <v-layout>
-        <v-flex>
-            <v-card :raised="raised" :elevation="elevation">
-                <v-card-text>
-                    <b>Lecturer:</b> {{ this.lecturer.name }}<br />
-                    <b>Exam Count:</b> {{ this.lecturer.exam_count }}<br />
-                    <b>Student Count:</b> {{ this.lecturer.student_count }}<br />
-                </v-card-text>
-            </v-card>
-            <v-card :raised="raised" :elevation="elevation">
-                <v-card-title class="py-4">
-                    <v-flex xs12 sm6 md3>
-                        <v-text-field
-                            v-model="search"
-                            append-icon="search"
-                            label="Search"
-                            single-line
-                            outlined
-                            :v-model="search"
-                            @input="filterData()"
-                        ></v-text-field>
-                    </v-flex>
-                </v-card-title>
-                <v-data-table disable-sort hide-default-footer :headers="headers" :items="data" class="calendar">
-                    <template v-slot:item="{ headers, item }">
-                        <tr>
-                            <td v-for="header in headers" v-html="item[header.value]" align="left" valign="top"></td>
-                        </tr>
-                    </template>
-                </v-data-table>
-                <v-snackbar v-model="snack" :timeout="3000" :color="snackColor">
-                    {{ snackText }}
-                    <v-btn text @click="snack = false">Close</v-btn>
-                </v-snackbar>
-            </v-card>
-        </v-flex>
+      <v-flex>
+        <v-card :raised="raised" :elevation="elevation">
+          <v-card-text>
+            <b>Lecturer:</b>
+            {{ this.lecturer.name }}
+            <br />
+            <b>Exam Count:</b>
+            {{ this.lecturer.exam_count }}
+            <br />
+          </v-card-text>
+        </v-card>
+        <v-card :raised="raised" :elevation="elevation">
+          <v-card-title class="py-4">
+            <v-flex xs12 sm6 md9>
+              <h5>AI CALENDAR</h5>
+            </v-flex>
+            <v-flex xs12 sm6 md3>
+              <v-text-field
+                append-icon="search"
+                label="Search"
+                single-line
+                outlined
+                v-model="ai_search"
+                @input="searchInData('ai')"
+              ></v-text-field>
+            </v-flex>
+          </v-card-title>
+          <v-card-text>
+            <v-data-table
+              disable-sort
+              hide-default-footer
+              :headers="ai_headers"
+              :items="ai_data"
+              class="calendar"
+            >
+              <template v-slot:item="{ headers, item }">
+                <tr>
+                  <td
+                    v-for="(header, i) in headers"
+                    v-bind:key="i"
+                    v-html="item[header.value]"
+                    align="left"
+                    valign="top"
+                  ></td>
+                </tr>
+              </template>
+            </v-data-table>
+          </v-card-text>
+        </v-card>
+        <v-card :raised="raised" :elevation="elevation">
+          <v-card-title class="py-4 pt-5 mt-5">
+            <v-flex xs12 sm6 md9>
+              <h5>MANUAL CALENDAR</h5>
+            </v-flex>
+            <v-flex xs12 sm6 md3>
+              <v-text-field
+                append-icon="search"
+                label="Search"
+                single-line
+                outlined
+                v-model="manual_search"
+                @input="searchInData('manual')"
+              ></v-text-field>
+            </v-flex>
+          </v-card-title>
+          <v-card-text>
+            <v-data-table
+              disable-sort
+              hide-default-footer
+              :headers="manual_headers"
+              :items="manual_data"
+              class="calendar"
+            >
+              <template v-slot:item="{ headers, item }">
+                <tr>
+                  <td
+                    v-for="(header, i) in headers"
+                    v-bind:key="i"
+                    v-html="item[header.value]"
+                    align="left"
+                    valign="top"
+                  ></td>
+                </tr>
+              </template>
+            </v-data-table>
+          </v-card-text>
+        </v-card>
+      </v-flex>
     </v-layout>
-</v-container>
+  </v-container>
 </template>
 
 <script>
-var _ = require('lodash');
+var _ = require("lodash");
 
 export default {
-    data: function() {
-        return {
-            search: '',
-            on: false,
-            elevation: 0,
-            raised: false,
-            snack: false,
-            snackColor: '',
-            snackText: '',
-            headers: [],
-            data: [],
-            tmp_data: [],
-            lecturer: []
+  data: function() {
+    return {
+      ai_search: "",
+      manual_search: "",
+      elevation: 0,
+      raised: false,
+      snack: false,
+      snackColor: "",
+      snackText: "",
+      ai_headers: [],
+      manual_headers: [],
+      ai_data: [],
+      manual_data: [],
+      tmp_ai_data: [],
+      tmp_manual_data: [],
+      lecturer: []
+    };
+  },
+  created: function() {
+    this.item_id = this.$router.currentRoute.params.id;
+    this.getData();
+  },
+  methods: {
+    getData() {
+      this.$http
+        .get(utils.apiBaseUrl + "lecturers/" + this.item_id)
+        .then(resp => {
+          this.lecturer = resp.data.data.lecturer;
+          this.tmp_ai_data = resp.data.data.calendars.ai;
+          this.tmp_manual_data = resp.data.data.calendars.manual;
+          this.preparingData(this.tmp_ai_data, "ai");
+          this.preparingData(this.tmp_manual_data, "manual");
+        })
+        .catch(err => {
+          this.snack = true;
+          this.snackColor = "error";
+          this.snackText = "Opps! Something went wrong when getting data.";
+        });
+    },
+    preparingData(exam_data, data_type) {
+      var new_data = [];
+      var new_headers = [];
+      new_headers.push({ text: "Hours", value: "hours", align: "center" });
+
+      var exams = exam_data;
+
+      for (var hour in exams) {
+        const obj = {};
+        const days = exams[hour];
+        for (var day in days) {
+          if (hour == 9) {
+            new_headers.push({ text: day, value: day });
+          }
+
+          obj["hours"] = hour;
+          var filtered = days[day].filter(function(el) {
+            return el != null;
+          });
+          obj[day] = filtered.join("<br>");
         }
-    },
-    created: function() {
-        this.item_id = this.$router.currentRoute.params.id;
-        this.getData();
-    },
-    methods: {
-        getData(){
-            this.$http
-            .get(utils.apiBaseUrl + "lecturers/" + this.item_id)
-            .then(resp => {
-                this.tmp_data = resp.data.data.exams;
-                this.lecturer = resp.data.data.lecturer;
-                this.preparingData(this.tmp_data);
-            })
-            .catch(err => {
-                this.snack = true;
-                this.snackColor = "error";
-                this.snackText = "Opps! Something went wrong when getting data.";
-            });
-        },
-        preparingData(exam_data){
-            
-            var new_data = [];
-            var new_headers = [];
-            new_headers.push({text: 'Hours', value: 'hours', align: 'center'});
-            
-            var exams = exam_data;
-            for(var hour in exams) {
-                
-                var obj = {};
-                const days = exams[hour];
-                for(var day in days){
+        new_data.push(obj);
+      }
 
-                    if(hour == 9){
-                        new_headers.push({ text: day, value: day });
-                    }
+      if (data_type == "ai") {
+        this.ai_headers = new_headers;
+        this.ai_data = new_data;
+      } else if (data_type == "manual") {
+        this.manual_headers = new_headers;
+        this.manual_data = new_data;
+      }
+    },
+    searchInData(data_type) {
+      var data = null;
+      var keyword = null;
 
-                    obj['hours'] = hour;
-                    var filtered = days[day].filter(function (el) {
-                        return el != null;
-                    });
-                    obj[day] = filtered.join('<br>');
-            
-                }
-                new_data.push(obj);
+      if (data_type == "ai") {
+        data = _.cloneDeep(this.tmp_ai_data);
+        keyword = this.ai_search;
+      } else if (data_type == "manual") {
+        data = _.cloneDeep(this.tmp_manual_data);
+        keyword = this.manual_search;
+      }
+
+      for (var hour in data) {
+        const days = data[hour];
+        for (var day in days) {
+          const exams = days[day];
+          for (var exam in exams) {
+            if (
+              exams[exam].toLowerCase().indexOf(keyword.toLowerCase()) === -1
+            ) {
+              delete data[hour][day][exam];
             }
-
-            this.headers = new_headers;
-            this.data = new_data;
-        },
-        filterData(){
-            var data = _.cloneDeep(this.tmp_data);
-            if(this.search != ''){
-                
-                for (var hour in data){
-                    const days = data[hour];
-                    for(var day in days){
-                        const exams = days[day];
-                        for(var exam in exams){
-                            if (exams[exam] != this.search){
-                                delete data[hour][day][exam];
-                            }
-                        }
-                    }
-                }
-                
-            }
-            this.preparingData(data);
-        },
-        save() {
-            this.snack = true
-            this.snackColor = 'success'
-            this.snackText = 'Data saved'
-        },
-        cancel() {
-            this.snack = true
-            this.snackColor = 'error'
-            this.snackText = 'Canceled'
-        },
-        open() {
-            this.snack = true
-            this.snackColor = 'info'
-            this.snackText = 'Dialog opened'
-        },
-        close() {}
+          }
+        }
+      }
+      this.preparingData(data, data_type);
     },
-}
+    save() {
+      this.snack = true;
+      this.snackColor = "success";
+      this.snackText = "Data saved";
+    },
+    cancel() {
+      this.snack = true;
+      this.snackColor = "error";
+      this.snackText = "Canceled";
+    },
+    open() {
+      this.snack = true;
+      this.snackColor = "info";
+      this.snackText = "Dialog opened";
+    },
+    close() {}
+  }
+};
 </script>
